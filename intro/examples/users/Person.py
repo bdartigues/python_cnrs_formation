357b5da0b9d24521aad7a3afc035b2755e3eb9f3
Alain Miniussi <alain@miniussi.net>
2016-11-14 09:20:56 +0100

    first draft at person

---
+++ intro/examples/users/Person.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+class Person:
+    def __init__(self, first_name, last_name, birth_date, desc):
+        self.first_name = first_name
+        self.last_name  = last_name
+        
+        
